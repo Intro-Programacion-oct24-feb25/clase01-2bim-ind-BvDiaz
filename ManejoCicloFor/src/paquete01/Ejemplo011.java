42f82443480b152d95d008cf122e0976cbf72947
Bryan Diaz <vicentepomas.2007@gmail.com>
2024-12-05 11:07:47 -0500

    PROBLEMA07

--- ManejoCicloFor/src/paquete01/Ejemplo011.java
+++ ManejoCicloFor/src/paquete01/Ejemplo011.java
@@ -16,6 +16,7 @@ public class Ejemplo011 {
      */
     public static void main(String[] args) {
         
+        
         for (int i = 10; i >= 0; i--) {
             System.out.printf("%d\n", i);
         }
